621c615e52c64e9495d1721d237f213852d3c8b9
Joe Ortega <joeortega79@gmail.com>
2018-03-22 09:43:14 +1030

    signup and login is implemented and functional.

--- app/src/main/java/com/udemy/sbsapps/whistlr/MainActivity.java
+++ app/src/main/java/com/udemy/sbsapps/whistlr/MainActivity.java
@@ -2,16 +2,59 @@ package com.udemy.sbsapps.whistlr;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.parse.LogInCallback;
 import com.parse.ParseAnalytics;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText usernameEditText, passwordEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        usernameEditText = findViewById(R.id.nameEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+
+        String title = "Whistlr: Login";
+        setTitle(title);
+
         ParseAnalytics.trackAppOpenedInBackground(getIntent());
+    }
+
+    public void signupLogin(View view) {
+        ParseUser.logInInBackground(usernameEditText.getText().toString(), passwordEditText.getText().toString(), new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if(e == null) {
+                    Log.i("Login", "Success!");
+                } else {
+                    e.printStackTrace();
+                    ParseUser newUser = new ParseUser();
+                    newUser.setUsername(usernameEditText.getText().toString());
+                    newUser.setPassword(passwordEditText.getText().toString());
+
+                    newUser.signUpInBackground(new SignUpCallback() {
+                        @Override
+                        public void done(ParseException e) {
+                            if(e == null) {
+                                Log.i("Signup", "Success!");
+                            } else {
+                                Toast.makeText(MainActivity.this, e.getMessage().substring(e.getMessage().indexOf(" ")), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
     }
 }
