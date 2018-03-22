d64598bb55d48378ae1c8bf28979cbe736bc20b9
Joe Ortega <joeortega79@gmail.com>
2018-03-22 12:21:55 +1030

    We can now logout and send Whistles and have them identified as from the user.

--- app/src/main/java/com/udemy/sbsapps/whistlr/UsersActivity.java
+++ app/src/main/java/com/udemy/sbsapps/whistlr/UsersActivity.java
@@ -1,19 +1,29 @@
 package com.udemy.sbsapps.whistlr;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.CheckedTextView;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.parse.FindCallback;
 import com.parse.Parse;
 import com.parse.ParseException;
+import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -78,5 +88,60 @@ public class UsersActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = new MenuInflater(this);
+
+        menuInflater.inflate(R.menu.whistlr_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(item.getItemId() == R.id.whistle) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Do a Whistle");
+
+            final EditText whistleEditText = new EditText(this);
+            builder.setView(whistleEditText);
+
+            builder.setPositiveButton("Send", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Log.i("Info", whistleEditText.getText().toString());
+
+                    ParseObject whistle = new ParseObject("Whistle");
+                    whistle.put("whistle", whistleEditText.getText().toString());
+                    whistle.put("username", ParseUser.getCurrentUser().getUsername());
+
+                    whistle.saveInBackground(new SaveCallback() {
+                        @Override
+                        public void done(ParseException e) {
+                            if(e == null){
+                                Toast.makeText(UsersActivity.this, "You whistled", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(UsersActivity.this, "Whistle was unsuccesful", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            });
+
+            builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Log.i("Info", "I dont want to whistle");
+                    dialog.cancel();
+                }
+            });
+        } else if(item.getItemId() == R.id.logout) {
+            ParseUser.logOut();
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
